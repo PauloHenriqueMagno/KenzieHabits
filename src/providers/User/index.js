7551362c08f0da661a8ce69b1c1a4acae99f115f
Sidney Patez <instalers.sa@gmail.com>
2021-10-15 09:36:51 -0300

    feat: user context features

--- src/providers/User/index.js
+++ src/providers/User/index.js
@@ -1,13 +1,26 @@
 import { createContext, useState } from "react";
+import api from "../../services/api";
 
 export const UserContext = createContext([]);
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState({});
 
-  const addUser = () => {};
+  const addUser = (user) => {
+    setUser(user);
+  };
 
-  const editUser = () => {};
+  const editUser = (editedUser) => {
+    setUser(editedUser);
+    api
+      .patch(`/users/${user.id}/`, editedUser, {
+        headers: {
+          Authorization: `Bearer ${user.access}`,
+        },
+      })
+      .then()
+      .catch((err) => console.log(err));
+  };
 
   return (
     <UserContext.Provider value={{ user, addUser, editUser }}>
