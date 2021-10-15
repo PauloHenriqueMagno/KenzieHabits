755fda9169674b95698eb071fe5b3c23f31ec523
Sidney Patez <instalers.sa@gmail.com>
2021-10-15 16:14:31 -0300

    feat: input features

--- src/components/Inputs/index.jsx
+++ src/components/Inputs/index.jsx
@@ -1,9 +1,19 @@
-import TextField from '@mui/material/TextField';
+// import TextField from '@mui/material/TextField';
+import { TextField } from "@material-ui/core";
 
-const Inputs = ({text}) =>{
-    return (
-        <TextField fullWidth color='primary' variant='outlined' label={text} />
-    );
+const Inputs = ({ text, register, name, error = null, ...rest }) => {
+  return (
+    <TextField
+      fullWidth
+      color="primary"
+      variant="outlined"
+      label={text}
+      {...register(name)}
+      error={!!error}
+      helperText={error}
+      {...rest}
+    />
+  );
 };
 
 export default Inputs;
