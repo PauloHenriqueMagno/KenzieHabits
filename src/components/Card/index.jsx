771701366f4d582013821f24d3edd27ebc044190
deborapavani <de.pavani@icloud.com>
2021-10-15 13:50:25 -0300

    feat: add progress bar

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -8,14 +8,47 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 
+import { useContext } from "react";
+// import { HabitsContext } from "../../providers/Habits";
+
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.mode === "light" ? "#F36A21" : "#F36A21",
+  },
+}));
+
 const Card = () => {
+  // const { habit, setHabit } = useContext(HabitsContext);
   return (
     <Card sx={{ maxWidth: 350 }}>
       <CardContent>
-        {/* Calistenia a tarde (15 minutos) <br />
-        Dificil <br />
-        Diariamente */}
+        {/* {habit.map((habit, index) => (
+          <div key={index}>
+            <p> {habit.title}</p>
+            <p> {habit.category}</p>
+            <p> {habit.difficulty}</p>
+            <p> {habit.frequency}</p>
+          </div>
+        ))} */}
+        Olá!
       </CardContent>
+      <Box sx={{ flexGrow: 1 }}>
+        <br />
+        <BorderLinearProgress variant="determinate" value={20} />
+      </Box>
       <FormGroup>
         <FormControlLabel
           control={<Checkbox defaultChecked />}
