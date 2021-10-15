337004c3c3be2007184d8d856c48aef9cb123a3a
deborapavani <de.pavani@icloud.com>
2021-10-15 11:04:03 -0300

    feat: importing components from mui

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,5 +1,38 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import AddIcon from "@mui/icons-material/Add";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+
 const Card = () => {
-  return <></>;
+  return (
+    <Card sx={{ maxWidth: 350 }}>
+      <CardContent>
+        {/* Calistenia a tarde (15 minutos) <br />
+        Dificil <br />
+        Diariamente */}
+      </CardContent>
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox defaultChecked />}
+          label="Concluído"
+        />
+      </FormGroup>
+      <CardActions>
+        <IconButton aria-label="add" size="small">
+          <AddIcon fontSize="small" />
+        </IconButton>
+
+        <IconButton aria-label="delete" size="small">
+          <EditIcon fontSize="small" />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Card;
