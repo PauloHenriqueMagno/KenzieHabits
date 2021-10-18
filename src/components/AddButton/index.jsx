f334085d0644da0e92f69e0fde1e4a9761055b1b
deborapavani <de.pavani@icloud.com>
2021-10-18 14:30:28 -0300

    feat: initial settings

--- src/components/AddButton/index.jsx
+++ src/components/AddButton/index.jsx
@@ -1,8 +1,14 @@
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+
 const AddButton = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return (
+    <>
+      <AddCircleOutlineIcon
+        onClick={() => alert("encaminha para o modal ou página")}
+        fontSize="large"
+      />
+    </>
+  );
+};
 
 export default AddButton;
