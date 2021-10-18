e02435e94d401ff2da6bbcdef491b932b8424f84
deborapavani <de.pavani@icloud.com>
2021-10-18 12:17:04 -0300

    feat: style.js created

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,51 +3,14 @@ import {
   IconButton,
   FormGroup,
   FormControlLabel,
-  Card,
   CardActions,
-  CardContent,
   Checkbox,
   Box,
-  styled,
 } from "@material-ui/core";
+import { StyledCard, BorderLinearProgress } from "./style.js";
 
 import { useContext } from "react";
 import { HabitsContext } from "../../providers/Habits";
-
-import LinearProgress, {
-  linearProgressClasses,
-} from "@material-ui/core/LinearProgress";
-import { orange } from "@material-ui/core/colors";
-
-const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-  height: 25,
-  borderRadius: 30,
-  [`&.${linearProgressClasses.colorPrimary}`]: {
-    backgroundColor:
-      theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
-  },
-  [`& .${linearProgressClasses.bar}`]: {
-    borderRadius: 30,
-    backgroundColor: theme.palette.mode === "light" ? "#F36A21" : "#F36A21",
-  },
-}));
-
-const StyledCard = styled(Card)({
-  boxShadow: "inset 0 0 1em rgba(0, 0, 0, 0.2)",
-  textTransform: "none",
-  fontSize: 16,
-  padding: "6px 12px",
-  border: "1px solid",
-  lineHeight: 1.5,
-  backgroundColor: "#5D6A5A",
-  borderColor: "#000000",
-  color: " #ffffff",
-  maxWidth: "300px",
-
-  span: {
-    color: " #F36A21",
-  },
-});
 
 const Cards = () => {
   const { habits, deleteHabit, editHabit } = useContext(HabitsContext);
