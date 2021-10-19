02616e9064a9ab4aeedd2e0f65563d4fdf4ae505
deborapavani <de.pavani@icloud.com>
2021-10-19 01:19:02 -0300

    feat: stylized button

--- src/components/AddButton/index.jsx
+++ src/components/AddButton/index.jsx
@@ -1,18 +1,15 @@
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import { styled } from "@material-ui/core";
 
-export const StyledCard = styled("div")({
+const StyledAddButton = styled("div")({
   color: " #F36A21",
 });
 
 const AddButton = () => {
   return (
-    <div>
-      <AddCircleOutlineIcon
-        onClick={() => alert("encaminha para o modal ou página")}
-        fontSize="large"
-      />
-    </div>
+    <StyledAddButton>
+      <AddCircleOutlineIcon fontSize="large" />
+    </StyledAddButton>
   );
 };
 
