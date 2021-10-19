1d50e90eba6f9e6f075985cb1782a30d268caf18
Sidney Patez <instalers.sa@gmail.com>
2021-10-19 09:11:54 -0300

    feat: page initial styles

--- src/pages/Groups/index.jsx
+++ src/pages/Groups/index.jsx
@@ -1,22 +1,26 @@
 import {
-  Accordion,
   AccordionActions,
-  AccordionDetails,
-  AccordionSummary,
-  Button,
-  Paper,
   Typography,
   List,
   ListItem,
   ListItemText,
-  TablePagination,
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { useState, useContext, useEffect } from "react";
-import { Container, StyledPaper, StyledBox } from "./styles";
+import {
+  Container,
+  StyledPaper,
+  StyledBox,
+  Content,
+  StyledTablePagination,
+  AccordionSummaryText,
+  StyledAccordion,
+  StyledButton,
+  StyledAccordionDetails,
+  StyledAccordionSummary,
+} from "./styles";
 
 import { GroupsContext } from "../../providers/Groups";
-import { Box } from "@material-ui/system";
 
 const Groups = () => {
   const [expanded, setExpanded] = useState(false);
@@ -42,110 +46,106 @@ const Groups = () => {
   };
   return (
     <Container>
-      <StyledPaper>
-        <StyledBox>
-          <Typography sx={{ width: "25%", flexShrink: 0 }}>Nome:</Typography>
-          <Typography sx={{ width: "25%", flexShrink: 0 }}>
-            Descrição:
-          </Typography>
-          <Typography sx={{ width: "25%", flexShrink: 0 }}>
-            Categoria:
-          </Typography>
-          <TablePagination
-            sx={{ color: "white" }}
-            component="div"
-            count={groups.count}
-            page={page}
-            onPageChange={handlePage}
-            rowsPerPage={15}
-            rowsPerPageOptions={[15]}
-          />
-        </StyledBox>
-        {groups.results?.map((group) => {
-          return (
-            <Accordion
-              key={group.id}
-              expanded={expanded === group.id}
-              onChange={handleChange(group.id)}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1bh-content"
-                id="panel1bh-header"
+      <Content>
+        <StyledPaper>
+          <StyledBox>
+            <div className="groupsListTitles">
+              <Typography>Nome:</Typography>
+            </div>
+            <div className="groupsListTitles">
+              <Typography>Descrição:</Typography>
+            </div>
+            <div className="groupsListTitles">
+              <Typography>Categoria:</Typography>
+            </div>
+            <StyledTablePagination
+              component="div"
+              count={groups.count}
+              page={page}
+              onPageChange={handlePage}
+              rowsPerPage={15}
+              rowsPerPageOptions={[15]}
+            />
+          </StyledBox>
+          {groups.results?.map((group) => {
+            return (
+              <StyledAccordion
+                key={group.id}
+                expanded={expanded === group.id}
+                onChange={handleChange(group.id)}
               >
-                <Typography sx={{ width: "25%", flexShrink: 0 }}>
-                  {group.name}
-                </Typography>
-                <Typography
-                  sx={{ color: "text.secondary", width: "25%", flexShrink: 0 }}
+                <StyledAccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls={group.id}
+                  id={group.id}
                 >
-                  {group.description}
-                </Typography>
-                <Typography sx={{ width: "25%", flexShrink: 0 }}>
-                  {group.category}
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography
-                  sx={{ display: "flex", justifyContent: "space-between" }}
-                >
-                  <div>
-                    <Typography>Atividades disponíveis:</Typography>
-                    <List>
-                      {group.activities.length !== 0 ? (
-                        group.activities.map((activitie) => {
-                          return (
-                            <ListItem>
-                              <ListItemText primary={activitie.title} />
-                            </ListItem>
-                          );
-                        })
-                      ) : (
-                        <Typography>
-                          Nenhuma atividade disponivel no momento
-                        </Typography>
-                      )}
-                    </List>
-                  </div>
-                  <div>
-                    <Typography>Objetivos do grupo:</Typography>
-                    <List>
-                      {group.goals.length !== 0 ? (
-                        group.goals.map((goal) => {
-                          return (
-                            <ListItem>
-                              <ListItemText primary={goal.title} />
-                            </ListItem>
-                          );
-                        })
-                      ) : (
-                        <Typography>
-                          Nenhuma atividade disponivel no momento
-                        </Typography>
-                      )}
-                    </List>
-                  </div>
-                  <div>
-                    <Typography>
-                      Este grupo tem {group.users_on_group.length} membros
-                      ativos no momento
-                    </Typography>
-                  </div>
-                </Typography>
-              </AccordionDetails>
-              <AccordionActions>
-                <Button
-                  size="small"
-                  color="primary"
-                  onClick={() => subscribeToGroup(group.id)}
-                >
-                  Participar deste grupo
-                </Button>
-              </AccordionActions>
-            </Accordion>
-          );
-        })}
-      </StyledPaper>
+                  <AccordionSummaryText>{group.name}</AccordionSummaryText>
+                  <AccordionSummaryText>
+                    {group.description}
+                  </AccordionSummaryText>
+                  <AccordionSummaryText>{group.category}</AccordionSummaryText>
+                </StyledAccordionSummary>
+                <StyledAccordionDetails>
+                  <Typography
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <div>
+                      <Typography>Atividades disponíveis:</Typography>
+                      <List>
+                        {group.activities.length !== 0 ? (
+                          group.activities.map((activitie) => {
+                            return (
+                              <ListItem>
+                                <ListItemText primary={activitie.title} />
+                              </ListItem>
+                            );
+                          })
+                        ) : (
+                          <Typography>
+                            Nenhuma atividade disponivel no momento
+                          </Typography>
+                        )}
+                      </List>
+                    </div>
+                    <div>
+                      <Typography>Objetivos do grupo:</Typography>
+                      <List>
+                        {group.goals.length !== 0 ? (
+                          group.goals.map((goal) => {
+                            return (
+                              <ListItem>
+                                <ListItemText primary={goal.title} />
+                              </ListItem>
+                            );
+                          })
+                        ) : (
+                          <Typography>
+                            Nenhuma atividade disponivel no momento
+                          </Typography>
+                        )}
+                      </List>
+                    </div>
+                    <div>
+                      <Typography>
+                        Este grupo tem {group.users_on_group.length} membros
+                        ativos no momento
+                      </Typography>
+                    </div>
+                  </Typography>
+                </StyledAccordionDetails>
+                <AccordionActions>
+                  <StyledButton
+                    size="small"
+                    onClick={() => subscribeToGroup(group.id)}
+                  >
+                    Participar deste grupo
+                  </StyledButton>
+                </AccordionActions>
+              </StyledAccordion>
+            );
+          })}
+        </StyledPaper>
+      </Content>
     </Container>
   );
 };
--- src/pages/Groups/styles.js
+++ src/pages/Groups/styles.js
@@ -1,3 +1,90 @@
-import { styled } from "@material-ui/core";
+import {
+  styled,
+  Accordion,
+  AccordionActions,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  TablePagination,
+  Box,
+} from "@material-ui/core";
 
-export const Container = styled("main")({});
+export const Container = styled("main")({
+  width: "100vw",
+});
+
+export const Content = styled("div")({
+  padding: "30px",
+  backgroundColor: "#F2F2F2",
+  maxWidth: "1280px",
+  width: "90%",
+  margin: "50px auto",
+  minHeight: "90vh",
+  borderRadius: "1%",
+});
+
+export const StyledPaper = styled(Paper)({
+  padding: "30px",
+  backgroundColor: "#5D6A5A",
+  color: "#FFF",
+});
+
+export const StyledBox = styled(Box)({
+  display: "flex",
+  alignItems: "flex-end",
+  justifyContent: "space-between",
+
+  "& .groupsListTitles": {
+    minHeight: 53,
+    width: "24%",
+    display: "flex",
+    alignItems: "center",
+  },
+});
+
+export const StyledTablePagination = styled(TablePagination)({
+  color: "#FFF",
+  flex: 1,
+});
+
+export const AccordionSummaryText = styled(Typography)({
+  width: "25%",
+  flexShrink: 0,
+  fontWeight: "bold",
+});
+
+export const StyledAccordion = styled(Accordion)({
+  color: "#000",
+  backgroundColor: "#fff",
+  marginBottom: 10,
+
+  svg: {
+    backgroundColor: "#F36A21",
+    borderRadius: "100%",
+    color: "#FFF",
+  },
+});
+
+export const StyledAccordionSummary = styled(AccordionSummary)({
+  backgroundColor: "#C4C4C4",
+  color: "#FFF",
+});
+
+export const StyledAccordionDetails = styled(AccordionDetails)({
+  color: "#000",
+  backgroundColor: "#fff",
+});
+
+export const StyledButton = styled(Button)({
+  color: "#F36A21",
+  fontWeight: "bold",
+
+  ":hover": {
+    backgroundColor: "rgba(243, 106, 33, 0.1)",
+  },
+});
