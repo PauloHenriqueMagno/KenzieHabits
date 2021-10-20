d12bc87b8033f3ad515b60aa96bc8094d400bd24
pedromenimen <pedromenimen@gmail.com>
2021-10-20 15:59:23 -0300

    feat: start of component activity list

--- src/components/ActivitiesList/index.jsx
+++ src/components/ActivitiesList/index.jsx
@@ -1,8 +1,30 @@
-const ActivitiesList = () =>{
-    return (
-        <>
-        </>
-    )
-}
+import { useContext, useEffect } from "react";
+import { ActivitiesContext } from "../../providers/Activities";
+import Modal from "../Modal/index";
 
-export default ActivitiesList
+const ActivitiesList = ({ group }) => {
+  const { activities, getActivities } = useContext(ActivitiesContext);
+  useEffect(() => {
+    getActivities(group.id);
+  }, []);
+  console.log(activities.data?.results);
+  return (
+    <div>
+      <div>
+        <h2>Atividades do grupo - {group.name} </h2>
+      </div>
+      {/* activities.map(activity => {}) */}
+      <div>
+        <div>
+          <span>{group.activities[0].title}</span>
+          <span>{group.activities[0].realization_time}</span>
+        </div>
+        <div>
+          <Modal modalType="UpdateActivity" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActivitiesList;
