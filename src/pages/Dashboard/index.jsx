a53c978b14506ac8f459259e21fef9dbaad27b90
pedromenimen <pedromenimen@gmail.com>
2021-10-20 14:26:54 -0300

    fix: all data reaches the api function as object

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,17 +1,34 @@
 import Cards from "../../components/Card";
 import Header from "../../components/Header";
-import AddButton from "../../components/AddButton";
-import { Container } from "./style.js";
-
+import { Container, Content } from "./style.js";
+import { useContext, useEffect } from "react";
+import { HabitsContext } from "../../providers/Habits";
+import Modal from "../../components/Modal";
 const Dashboard = () => {
+  const { habits, getHabits, editHabit, deleteHabit } =
+    useContext(HabitsContext);
+
+  console.log(habits);
+
+  useEffect(() => {
+    getHabits();
+  }, []);
+
   return (
-    <>
+    <Container>
       <Header></Header>
-      <Container>
-        <Cards></Cards>
-      </Container>
-      <AddButton></AddButton>
-    </>
+      <Content>
+        {habits.map((habits, index) => (
+          <Cards
+            key={index}
+            habits={habits}
+            del={deleteHabit}
+            edit={editHabit}
+          />
+        ))}
+      </Content>
+      <Modal modalType="CreateHabit" />
+    </Container>
   );
 };
 
