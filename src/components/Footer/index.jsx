a29cc03c15c57300cc7a15921a31d279188bb093
Matheus-Henrique-Burey <matheushburei.99@gmail.com>
2021-10-21 16:18:00 -0300

    refatoração do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,16 @@
-import {FooterStyle} from './style.js';
+import {FooterStyle, A} from './style.js';
 
 const Footer = () => {
     return (
         <FooterStyle>
-            <p>Criado por: Matheus, Debora, Paulo, Pedro, Sidney</p>
+            <p>
+                Criado por: 
+                <a href='https://www.linkedin.com/in/d%C3%A9bora-pavani-193889a1/' target='_blank'> Débora Pavani</a>, 
+                <a href='https://www.linkedin.com/in/matheus-h-burei/' target='_blank'> Matheus H. Burei</a>, 
+                <a href='https://www.linkedin.com/in/paulohenriquemagno/' target='_blank'> Paulo H. Magno</a>, 
+                <a href='https://www.linkedin.com/in/pedro-henrique-b741ab197/' target='_blank'> Pedro H. Moreira</a>, 
+                <a href='https://www.linkedin.com/in/sidney-patez/' target='_blank'> Sidney Patez</a>
+            </p>
         </FooterStyle>
     )
 };
