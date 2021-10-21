b9de0da6611bc8007314581c9567f922528fb5cb
Sidney Patez <instalers.sa@gmail.com>
2021-10-21 10:48:37 -0300

    fix: activities list errors

--- src/components/ActivitiesList/index.jsx
+++ src/components/ActivitiesList/index.jsx
@@ -1,34 +1,28 @@
-import { useContext, useEffect } from "react";
-import { ActivitiesContext } from "../../providers/Activities";
 import Modal from "../Modal/index";
+import { Delete } from "@material-ui/icons";
 import { container } from "./styles";
 import { Box } from "@material-ui/core";
 import { theme } from "./styles";
 import { ThemeProvider } from "@material-ui/core";
 
-const ActivitiesList = ({ group }) => {
-  const { activities, getActivities } = useContext(ActivitiesContext);
-  useEffect(() => {
-    getActivities(group.id);
-  }, []);
-  console.log(activities.data?.results);
+const ActivitiesList = ({ group: { activities } }) => {
   return (
     <ThemeProvider theme={theme}>
       <Box sx={container}>
-        <div className="titleContainer">
-          <h4>Atividades do grupo - {group.name} </h4>
-          <Modal modalType="CreateActivity" />
-        </div>
-        {/* activities.map(activity => {}) */}
-        <div className="activityContainer">
-          <div>
-            <span>{group.activities[0].title}</span>
-            <span>{group.activities[0].realization_time}</span>
-          </div>
-          <div>
-            <Modal modalType="UpdateActivity" />
-          </div>
-        </div>
+        {activities.map((activity) => {
+          return (
+            <div className="activityContainer">
+              <div className="activityContent">
+                <p>{activity.title}</p>
+                <p>{activity.realization_time}</p>
+              </div>
+              <div className="activityActions">
+                <Delete />
+                <Modal modalType="UpdateActivity" activityId={activity.id} />
+              </div>
+            </div>
+          );
+        })}
       </Box>
     </ThemeProvider>
   );
