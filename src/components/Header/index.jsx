5ea04cccaec4253a02b7e42fc3f677c81671412c
pedromenimen <pedromenimen@gmail.com>
2021-10-21 21:46:34 -0300

    feat: popover logout edite user

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,14 +4,14 @@ import {
   Logo,
   IconUser,
 } from './style.js';
-import Modal from '../Modal';
+import UserPopover from '../popover/index.js';
 
 const Header = () => {
   return (
     <Container>
       <IconUser>
         <Logo to='/dashboard' >K<span>HABIT</span>Z</Logo>
-        <Modal modalType="UpdateUserName" />
+        <UserPopover/>
       </IconUser>
       <Button to='/dashboard' >Meus Habitos</Button>
       <Button to='/dashboard/groups' >Meus Grupos</Button>
---
+++ src/components/popover/styles.js
@@ -0,0 +1,12 @@
+export const buttonContainer = {
+  padding: "5px",
+  display: "flex",
+  button: {
+    width: "max-content",
+    margin: "2px",
+    backgroundColor: "#F36A21",
+    ":hover": {
+      backgroundColor: "white",
+    },
+  },
+};
